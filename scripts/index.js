7b6c85ef08e042205993d1804d431359ec6049c9
Ramona H <mjasepi.91@gmail.com>
2024-09-08 15:31:59 -0700

    adding js file

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+let initialCards = [
+  {
+    name: "St Augustine",
+    link: "https://images.unsplash.com/photo-1719425809677-72b27e85514e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Austin",
+    link: "https://images.unsplash.com/photo-1582938433294-56edbf8c685e?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Anaheim",
+    link: "https://images.unsplash.com/photo-1578302550612-52af93e5f20e?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Nashville",
+    link: "https://images.unsplash.com/photo-1537889230356-e49d1df471b9?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Brooklyn",
+    link: "https://images.unsplash.com/photo-1576608052974-15baeceeea7c?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Flagstaff",
+    link: "https://images.unsplash.com/photo-1619152045343-39b12e931f20?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
